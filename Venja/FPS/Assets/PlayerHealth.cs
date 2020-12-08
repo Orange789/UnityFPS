1ba7d2f75a5f43cfdc5bf38172f742677e173920
karlmagnuspensa <karlmagnus.pensa@gmail.com>
2020-12-08 17:57:52 +0200

    version.2

--- Venja/FPS/Assets/PlayerHealth.cs
+++ Venja/FPS/Assets/PlayerHealth.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int health;
+    public Slider myHealth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myHealth.value = 10;
     }
 
     // Update is called once per frame
@@ -20,6 +23,7 @@ public class PlayerHealth : MonoBehaviour
     public void DecreaseHealth()
     {
         health--;
+        myHealth.value--;
         if(health<= 0)
         {
             Application.LoadLevel(Application.loadedLevel);
